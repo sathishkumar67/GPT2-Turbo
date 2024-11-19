82d8d45a2e3edf32f8b5c17a541114c85c5d8e39
sathishkumar67 <sathishvillers2000@gmail.com>
2024-11-19 15:31:09 +0530

    prepared model

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 from dataclasses import dataclass
+import gin
+from typing import Tuple
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-import gin
-from typing import Tuple
 
 
 @gin.configurable
@@ -30,7 +30,7 @@ class GPTConfig:
 
 
 class RMSNorm(torch.nn.Module):
-    def __init__(self, dim: int, eps: float = 1e-6):
+    def __init__(self, dim: int, eps: float = 1e-6) -> None:
         """
         Initializes the RMSNorm module.
 
@@ -42,7 +42,7 @@ class RMSNorm(torch.nn.Module):
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(dim))
 
-    def _norm(self, x):
+    def _norm(self, x) -> torch.Tensor:
         """
         Computes the RMSNorm of a tensor.
 
@@ -57,7 +57,7 @@ class RMSNorm(torch.nn.Module):
         """
         return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
 
-    def forward(self, x):        
+    def forward(self, x) -> torch.Tensor:        
         """
         Computes the RMSNorm of a tensor and applies a learnable scale factor.
 
@@ -73,8 +73,21 @@ class RMSNorm(torch.nn.Module):
 
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
+        """
+        Initializes the CausalSelfAttention module.
+
+        Args:
+            config: The GPTConfig.
+
+        Notes:
+            The `config` object should have the following attributes:
+
+                - `n_head`: The number of attention heads.
+                - `n_embd`: The dimension of the input tensor.
+        """
         super().__init__()
         self.config = config
+        # check that the dimension is a multiple of n_head
         assert self.config.n_embd % self.config.n_head == 0
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(self.config.n_embd, 3 * self.config.n_embd)
@@ -82,6 +95,18 @@ class CausalSelfAttention(nn.Module):
         self.c_proj = nn.Linear(self.config.n_embd, self.config.n_embd)
         
     def forward(self, x):
+        """
+        Computes the causal self-attention of the input tensor.
+
+        Given an input tensor `x` of shape `(B, T, C)`, computes its causal self-attention
+        using the `scaled_dot_product_attention` function from PyTorch.
+
+        Args:
+            x: The input tensor.
+
+        Returns:
+            The causal self-attention of the input tensor.
+        """
         B, T, C = x.size()
         qkv = self.c_attn(x)
         q, k, v = qkv.split(self.config.n_embd, dim=2)
@@ -96,12 +121,31 @@ class CausalSelfAttention(nn.Module):
 class MLP(nn.Module):
 
     def __init__(self, config):
+        """Initializes the MLP module.
+
+        Args:
+            config: The GPTConfig.
+
+        Notes:
+            The `config` object should have the following attributes:
+
+                - `n_embd`: The dimension of the input tensor."""
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu    = nn.GELU(approximate='tanh')
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
 
     def forward(self, x):
+        """Computes the feedforward neural network of the input tensor.
+
+        Given an input tensor `x` of shape `(B, T, C)`, computes its feedforward neural
+        network using a linear layer, a GeLU activation function, and another linear layer.
+
+        Args:
+            x: The input tensor.
+
+        Returns:
+            The feedforward neural network of the input tensor."""
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
@@ -110,6 +154,11 @@ class MLP(nn.Module):
 class Block(nn.Module):
 
     def __init__(self, config):
+        """Initializes the Block module.
+
+        Given a GPTConfig object, initializes the Block module, which consists of
+        a causal self-attention layer, a layer normalization layer, a feedforward
+        neural network layer, and another layer normalization layer."""
         super().__init__()
         self.ln_1 = RMSNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
@@ -117,6 +166,17 @@ class Block(nn.Module):
         self.mlp = MLP(config)
 
     def forward(self, x):
+        """Computes the block of the transformer.
+
+        Given an input tensor `x` of shape `(B, T, C)`, computes its block using a
+        causal self-attention layer, a layer normalization layer, a feedforward
+        neural network layer, and another layer normalization layer.
+
+        Args:
+            x: The input tensor.
+
+        Returns:
+            The block of the input tensor."""
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
@@ -124,6 +184,16 @@ class Block(nn.Module):
 class GPT(nn.Module):
 
     def __init__(self, config):
+        """Initializes the GPT module.
+
+        Given a GPTConfig object, initializes the GPT module, which consists of an
+        embedding layer, a positional embedding layer, a stack of transformer
+        blocks, a layer normalization layer, and a linear layer for generating
+        logits. The weights of the embedding layer are shared with the weights of
+        the linear layer.
+
+        Args:
+            config: The GPTConfig object."""
         super().__init__()
         self.config = config
 
@@ -141,6 +211,26 @@ class GPT(nn.Module):
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
+        """
+        Computes the forward pass of the GPT model.
+
+        Given an input tensor `idx` of shape `(B, T)`, computes its forward pass
+        using the transformer blocks, layer normalization, and a linear layer for
+        generating logits.
+
+        Args:
+            idx: The input tensor.
+
+        Returns:
+            The forward pass of the input tensor.
+
+        Notes:
+            The `config` object should have the following attributes:
+
+                - `n_embd`: The dimension of the input tensor.
+                - `vocab_size`: The size of the vocabulary.
+                - `block_size`: The maximum block size of the input tensor.
+        """
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
         # forward the token and posisition embeddings
@@ -157,82 +247,4 @@ class GPT(nn.Module):
         loss = None
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
-        return logits, loss
-
-    @classmethod
-    def from_pretrained(cls, model_type):
-        """Loads pretrained GPT-2 model weights from huggingface"""
-        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
-        from transformers import GPT2LMHeadModel
-        print("loading weights from pretrained gpt: %s" % model_type)
-
-        # n_layer, n_head and n_embd are determined from model_type
-        config_args = {
-            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
-        }[model_type]
-        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
-        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
-        # create a from-scratch initialized minGPT model
-        config = GPTConfig(**config_args)
-        model = GPT(config)
-        sd = model.state_dict()
-        sd_keys = sd.keys()
-        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
-
-        # init a huggingface/transformers model
-        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-        sd_hf = model_hf.state_dict()
-
-        # copy while ensuring all of the parameters are aligned and match in names and shapes
-        sd_keys_hf = sd_hf.keys()
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
-        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
-        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
-        # this means that we have to transpose these weights when we import them
-        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
-        for k in sd_keys_hf:
-            if any(k.endswith(w) for w in transposed):
-                # special treatment for the Conv1D weights we need to transpose
-                assert sd_hf[k].shape[::-1] == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k].t())
-            else:
-                # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k])
-
-        return model
-    
-
-# import lightning as L
-
-# class GPT2Wrapper(L.LightningModule):
-#     def __init__(self, config: GPTConfig, model: GPT):
-#         super().__init__()
-#         self.config = config
-#         self.model = model    
-#         self.optimizer = self.configure_optimizers()
-#         self.train_loss = []
-
-#     def forward(self, idx, targets=None):
-#         return self.model(idx, targets)
-
-#     def training_step(self, batch, batch_idx):
-#         self.model.train()
-#         optimizer = self.optimizers()
-#         optimizer.zero_grad()
-        
-#         batch, label = batch
-#         _, loss = self.model(batch, label)
-#         self.log("Train_Loss", loss, prog_bar=True)
-#         self.train_loss.append(loss.item())
-
-#         return loss
-    
-#     def configure_optimizers(self):
-#         return torch.optim.AdamW(self.model.parameters(), lr=self.config.learning_rate, betas=self.config.betas, eps=self.config.eps, weight_decay=self.config.weight_decay)
+        return logits, loss 
