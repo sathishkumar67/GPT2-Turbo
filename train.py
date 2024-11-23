dd72df126371ae794c5122c52085f22a270871ed
sathishkumar67 <sathishvillers2000@gmail.com>
2024-11-23 15:15:21 +0530

    removed  extra worker

--- train.py
+++ train.py
@@ -99,7 +99,7 @@ def trainer(rank, world_size):
     # Use DistributedSampler to partition data among distributed processes
     sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=True, drop_last=True)
     # Use DataLoader to manage batches
-    dataloader = DataLoader(dataset, batch_size=config.batch_size, sampler=sampler, drop_last=True, num_workers=2, pin_memory=True, pin_memory_device=f"{device.type}:{rank}", prefetch_factor=2, persistent_workers=True)
+    dataloader = DataLoader(dataset, batch_size=config.batch_size, sampler=sampler, drop_last=True, num_workers=1, pin_memory=True, pin_memory_device=f"{device.type}:{rank}", prefetch_factor=2, persistent_workers=True)
     print(f"dataloader size: {len(dataloader)}")
 
 
