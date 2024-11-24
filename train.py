d56ad28e5cde826d34503797c4f54933c5208742
sathishkumar67 <sathishvillers2000@gmail.com>
2024-11-24 13:26:33 +0530

    changed token count

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ elif DO_DATASET_DOWNLOAD:
 
 
 # Load the dataset
-tokens = np.load(f"{LOCAL_DIR}/{DATA_FILENAME}", allow_pickle=True)[:30894465]
+tokens = np.load(f"{LOCAL_DIR}/{DATA_FILENAME}", allow_pickle=True)[:30894466]
 
 
 if LOAD_CHECKPOINT:
