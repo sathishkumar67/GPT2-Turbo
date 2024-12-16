eb2a8e448ba0bbe1a80a327ee7a9df81e6cc9511
sathishkumar67 <sathishvillers2000@gmail.com>
2024-12-16 16:53:29 +0530

    added 1extra token for labels

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ elif DO_DATASET_DOWNLOAD:
 
 
 # Load the training dataset and eval dataset
-tokens = np.load(f"{LOCAL_DIR}/{TRAIN_DATA_FILENAME}", allow_pickle=True)[585105410:611844098]
+tokens = np.load(f"{LOCAL_DIR}/{TRAIN_DATA_FILENAME}", allow_pickle=True)[585105410:611844099]
 eval_tokens = np.load(f"{LOCAL_DIR}/{EVAL_DATA_FILENAME}", allow_pickle=True)[2000000:2500000]
 print(f"Dataset loaded with {len(tokens)} tokens....")
 print(f"Evaluation Dataset loaded with {len(eval_tokens)} tokens....")
